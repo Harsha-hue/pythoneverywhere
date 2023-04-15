694e647d99caf9fa1ff677a5858340f5f688f247
Harsha-hue <58879096+Harsha-hue@users.noreply.github.com>
2023-04-15 06:29:46 +0530

    Update Ai2.0.py

--- Ai2.0.py
+++ Ai2.0.py
@@ -1,48 +1,39 @@
+import os
 import telegram
-import logging
-import openai_secret_manager
 import openai
 
-# Initialize the Telegram API
-secrets = openai_secret_manager.get_secret("telegram")
-bot = telegram.Bot(token=secrets["5829477521:AAH6_LfvVCpUDQBHBpZ6uzOwiIfQXYmOLvQ"])
+# Set up OpenAI API key
+openai.api_key = "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"
 
-# Initialize the OpenAI API
-secrets = openai_secret_manager.get_secret("openai")
-openai.api_key = secrets["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"]
+# Set up Telegram bot token
+bot = telegram.Bot(token="5829477521:AAH6_LfvVCpUDQBHBpZ6uzOwiIfQXYmOLvQ")
 
-# Set up logging
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                    level=logging.INFO)
-
-# Define a function to respond to messages
-def respond(update, context):
-    # Get the user's message
+# Define function to handle incoming messages
+def handle_message(update, context):
+    # Get message text and chat ID
     message = update.message.text
-
-    # Use OpenAI to generate a response
-    prompt = f"Q: {message}\nA:"
+    chat_id = update.message.chat_id
+    
+    # Call OpenAI GPT-3 API to generate response
     response = openai.Completion.create(
-        engine="davinci",
-        prompt=prompt,
-        max_tokens=1024,
+        engine="text-davinci-002",
+        prompt=message,
+        temperature=0.5,
+        max_tokens=50,
         n=1,
         stop=None,
-        temperature=0.5,
+        timeout=10,
     )
+    reply = response.choices[0].text.strip()
+    
+    # Send response back to Telegram chat
+    bot.send_message(chat_id=chat_id, text=reply)
 
-    # Send the response to the user
-    bot.send_message(chat_id=update.message.chat_id, text=response.choices[0].text)
+# Set up handler for incoming messages
+updater = telegram.ext.Updater(token="<YOUR_TELEGRAM_BOT_TOKEN>", use_context=True)
+dispatcher = updater.dispatcher
+dispatcher.add_handler(telegram.ext.MessageHandler(telegram.ext.Filters.text, handle_message))
 
 # Start the bot
-if __name__ == '__main__':
-    updater = telegram.ext.Updater(token=secrets["access_token"], use_context=True)
-    dispatcher = updater.dispatcher
-
-    # Add a handler to respond to messages
-    dispatcher.add_handler(telegram.ext.MessageHandler(telegram.ext.Filters.text, respond))
-
-    # Start the bot
-    updater.start_polling()
-    logging.info("Bot started")
-    updater.idle()
+updater.start_polling()
+updater.idle()
